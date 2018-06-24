2fc52ee351c98ccb43d03fcb016749db50930fe3
Lauren Ventura <laurenfventura@gmail.com>
2018-06-24 19:01:16 -0400

    added server code and html

---
+++ server.js
@@ -0,0 +1,48 @@
+//require dependencies
+var express = require("express");
+var mongoose = require("mongoose");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+
+//set up port
+var PORT = process.env.PORT || 3000;
+
+//instantiate our Express App
+var app = express();
+
+//express router
+var router = express.Router();
+
+//designate our public folder as static
+app.use(express.static(__dirname + "/public"));
+
+//connect Handlebars to app
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+//bodyparser
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(router);
+
+//localhost database, change when deployed
+var db = process.env.MONGODB.URI || "mongodb://localhost/mongoHeadlines";
+
+//connect to db
+mongoose.connect(db, function(error){
+    if (error){
+        console.log(error);
+    }
+    else{
+        console.log("mongoose connection is successful");
+    }
+});
+
+//listen on port
+app.listen(PORT, function() {
+    console.log("Listening on port:" + PORT);
+});
