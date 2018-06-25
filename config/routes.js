2e40e5ce8631ec880dfbcc3b16fbb67e54ca6724
Lauren Ventura <laurenfventura@gmail.com>
2018-06-24 20:33:13 -0400

    made changes to routes

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,27 @@
 var scrape = require("../scripts/scrape");
-
 var headlinesController = require("../controllers/headlines");
-
 var notesController = require("../controllers/notes");
 
 module.exports = function(router) {
-    router.get("/", function(req, res){
-        res.render("home");
-    });
+	router.get("/", function (req, res) {
+		res.render("home");
+	});
+	router.get("/saved", function(req, res) {
+		res.render("saved");
+	});
 
-    router.get("/saved", function(req, res) {
-        res.render("saved");
-    });
+	router.get("/api/fetch", function(req, res) {
+		headlinesController.fetch(function(err, docs) {
+			if (!docs || docs.insertedCount === 0) {
+				res.json({
+					message: "No new articles today."
+				});
+			}
+			else {
+				res.json({
+					message: "Added " + docs.insertedCount + "new articles"
+				});
+			}
+		});
+	});
 }
